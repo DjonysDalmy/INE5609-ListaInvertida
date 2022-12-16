519f44790c4d4987a127cb18464fbc6e910f55ec
Gui-Pinheiro10 <guilhermeifsc10@gmail.com>
2022-12-15 22:32:07 -0300

    modificações nos diretorios e em funcionalidades do programa

--- diretorio_discreto.py
+++ diretorio_discreto.py
@@ -1,6 +1,13 @@
 class DiretorioDiscreto:
-  def __init__(self, nome, id):
-    self.dados_diretorio = [nome, [id]]
+  def __init__(self):
+    self.dados_diretorio = []
+    self.chaves_cadastradas = []
 
-  def acrescentar_item_diretorio(self, id):
-    self.dados_diretorio[1].append(id)
+  def acrescentar_item_diretorio(self, dado, id):
+    if dado in self.chaves_cadastradas:
+      for i in range(0, len(self.dados_diretorio)):
+        if dado == self.dados_diretorio[i][0]:
+          self.dados_diretorio[i][1].append(id)
+    else:
+      self.dados_diretorio.append([f'{dado}', [id]])
+      self.chaves_cadastradas.append(dado)
--- lista_invertida.py
+++ lista_invertida.py
@@ -6,9 +6,9 @@ from time import sleep
 class Lista_Invertida:
   def __init__(self):
     self.lista_elementos = []
-    self.cidades = DiretorioDiscreto
-    self.times = DiretorioDiscreto
-    self.estilos_musicais = DiretorioContinuo
+    self.cidades = DiretorioDiscreto()
+    self.estilos_musicais = DiretorioDiscreto()
+    self.salarios = DiretorioContinuo()
 
   def verifica_resposta(self, pergunta):
     if pergunta.upper() == 'S':
@@ -24,16 +24,22 @@ class Lista_Invertida:
   def menu(self):
     sleep(3)
     print('\nEscolha uma das opções:')
-    print(('1 - Buscar por cidade\n2 - Buscar por estilo musical\n3 - Buscar por salário\n4 - Fazer busca composta\n5 - Incluir nova pessoa\n6 - Remover pessoa\n7 - Exibir todas as pessoas'))
+    print(('1 - Buscar por cidade\n2 - Buscar por estilo musical\n3 - Buscar por salário\n4 - Fazer busca composta\n5 - Incluir nova pessoa\n6 - Remover pessoa\n7 - Exibir todas as pessoas\n8 - Sair'))
     resposta = int(input('\nEscolha sua opção: '))
-    if resposta in [1, 2, 3, 4, 5, 6, 7]:
+    if resposta in [1, 2, 3, 4, 5, 6, 7, 8]:
       return resposta
     else:
       print('\nResposta não encontrada!')
       self.menu()
 
-  def buscar_por_criterio(self, index):
+  def menu_busca_composta(self):
     pass
+
+  def carregar_diretorios(self):
+    for elemento in self.lista_elementos:
+      self.cidades.acrescentar_item_diretorio(elemento.cidade, elemento.id)
+      self.estilos_musicais.acrescentar_item_diretorio(elemento.estilo_musical, elemento.id)
+      self.salarios.acrescentar_item_diretorio(elemento.salario, elemento.id)
 
   def verifica_lista_vazia(self):
     if len(self.lista_elementos) == 0:
@@ -42,36 +48,100 @@ class Lista_Invertida:
     else:
       return False
       
+  def adicionar_pessoa(self):
+    id = len(self.lista_elementos) + 1
+    nome = input('Informe o nome da nova pessoa: ')
+    cidade = input('Informe a cidade da nova pessoa: ')
+    estilo_musical = input('Informe o estilo musical da nova pessoa: ')
+    salario = input('Informe o salário da nova pessoa: ')
+    nova_pessoa = Elemento(id, nome, cidade, estilo_musical, salario)
+    self.lista_elementos.append(nova_pessoa)
+    self.cidades.acrescentar_item_diretorio(nova_pessoa.cidade, nova_pessoa.id)
+    self.estilos_musicais.acrescentar_item_diretorio(nova_pessoa.estilo_musical, nova_pessoa.id)
+    self.salarios.acrescentar_item_diretorio(nova_pessoa.salario, nova_pessoa.id)
+    print('Pessoa adicionada com sucesso!')
+
+  def remover_pessoa(self):
+    id_pessoa_excluida = int(input('Informe o ID da pessoa que quer excluir: '))
+    for pessoa in self.lista_elementos:
+      if id_pessoa_excluida == pessoa.id:
+        cidade_pessoa_excluida = pessoa.cidade
+        estilo_musical_pessoa_excluida = pessoa.estilo_musical
+        salario_pessoa_excluida = pessoa.salario
+        for cidade in self.cidades.dados_diretorio:
+          if cidade[0] == cidade_pessoa_excluida:
+            cidade[1].remove(id_pessoa_excluida)
+        for estilo_musical in self.estilos_musicais.dados_diretorio:
+          if estilo_musical[0] == estilo_musical_pessoa_excluida:
+            estilo_musical[1].remove(id_pessoa_excluida)
+        for salario in self.salarios.dados_diretorio:
+          if salario[0] == salario_pessoa_excluida:
+            salario[1].remove(id_pessoa_excluida)
+        self.lista_elementos.remove(pessoa)
+        print('Pessoa removida com sucesso!')
+
   def iniciar_programa(self):
     pergunta = input('Você deseja carregar a carga de dados? ')
     if self.verifica_resposta(pergunta):
       self.carregar_cargo_de_dados()
-      print('Aqui está a lista de elementos:')
-      for elemento in self.lista_elementos:
-        print(f'ID: {elemento.id} - Nome: {elemento.nome}, Cidade: {elemento.cidade}, Estilo Musical: {elemento.estilo_musical}, Salário: {elemento.salario}')
+      self.carregar_diretorios()
     else:
       print('\nOk! Vamos iniciar sem dados...')
-    resposta_menu = self.menu()
-    if resposta_menu == 1:
-      if self.verifica_lista_vazia() is False:
-        pass
-      else:
-        print('teste')
-    elif resposta_menu == 2:
-      if self.verifica_lista_vazia() is False:
-        pass
-    elif resposta_menu == 3:
-      if self.verifica_lista_vazia() is False:
-        pass
-    elif resposta_menu == 4:
-      if self.verifica_lista_vazia() is False:
-        pass
-    elif resposta_menu == 5:
-      if self.verifica_lista_vazia() is False:
-        pass
-    elif resposta_menu == 6:
-      if self.verifica_lista_vazia() is False:
-        pass
-    elif resposta_menu == 7:
-      if self.verifica_lista_vazia() is False:
-        pass
+    resposta_menu = ''
+    while resposta_menu != 8:
+      resposta_menu = self.menu()
+      if resposta_menu == 1:
+        if self.verifica_lista_vazia() is False:
+          for i in range(0 , len(self.cidades.chaves_cadastradas)):
+            print(f'{i+1} - {self.cidades.chaves_cadastradas[i]}')
+          cidade_escolhida = self.cidades.chaves_cadastradas[int(input('\nEscolha sua opção: '))-1]
+          print(f'\nAs pessoas de {cidade_escolhida} são: ')
+          sleep(1)
+          for cidade in self.cidades.dados_diretorio:
+            if cidade_escolhida == cidade[0]:
+              for id in cidade[1]:
+                for pessoa in self.lista_elementos:
+                  if id == pessoa.id:
+                    print(pessoa.nome)
+        else:
+          print('teste')
+      elif resposta_menu == 2:
+        if self.verifica_lista_vazia() is False:
+          for i in range(0 , len(self.estilos_musicais.chaves_cadastradas)):
+            print(f'{i+1} - {self.estilos_musicais.chaves_cadastradas[i]}')
+          estilo_escolhido = self.estilos_musicais.chaves_cadastradas[int(input('\nEscolha sua opção: '))-1]
+          print(f'\nAs pessoas do {estilo_escolhido} são: ')
+          sleep(1)
+          for estilo_musical in self.estilos_musicais.dados_diretorio:
+            if estilo_escolhido == estilo_musical[0]:
+              for id in estilo_musical[1]:
+                for pessoa in self.lista_elementos:
+                  if id == pessoa.id:
+                    print(pessoa.nome)
+      elif resposta_menu == 3:
+        if self.verifica_lista_vazia() is False:
+          for i in range(0 , len(self.salarios.chaves_cadastradas)):
+            print(f'{i+1} - {self.salarios.chaves_cadastradas[i]}')
+          salario_escolhido = self.salarios.chaves_cadastradas[int(input('\nEscolha sua opção: '))-1]
+          print(f'\nAs pessoas de salário {salario_escolhido} são: ')
+          sleep(1)
+          for salario in self.salarios.dados_diretorio:
+            if salario_escolhido == float(salario[0]):
+              for id in salario[1]:
+                for pessoa in self.lista_elementos:
+                  if id == pessoa.id:
+                    print(pessoa.nome)
+      elif resposta_menu == 4:
+        if self.verifica_lista_vazia() is False:
+          pass
+      elif resposta_menu == 5:
+        if self.verifica_lista_vazia() is False:
+          self.adicionar_pessoa()
+      elif resposta_menu == 6:
+        if self.verifica_lista_vazia() is False:
+          self.remover_pessoa()
+      elif resposta_menu == 7:
+        if self.verifica_lista_vazia() is False:
+          print('Aqui está a lista de elementos:')
+          for elemento in self.lista_elementos:
+            print(f'ID: {elemento.id} - Nome: {elemento.nome}, Cidade: {elemento.cidade}, Estilo Musical: {elemento.estilo_musical}, Salário: {elemento.salario}')
